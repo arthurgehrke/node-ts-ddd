47702da8e5b53d29986d20b83f0e9ed2fa4c1e50
arthurgehrke <arthurgehrke07@gmail.com>
2020-07-09 11:17:41 -0300

    chore: add typeorm and pg dependencies and create database file

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import express from 'express';
 
 import routes from './routes';
+import './database';
 
 const app = express();
 
-app.listen(3000, () => 'Server running on 3000');
+app.listen(3000, () => {
+	console.log('Server running on 3000');
+});
