e857bb94a9c6c7d49d29041ca0f9e1cce1ba5c4b
arthurgehrke <arthurgehrke07@gmail.com>
2020-07-17 13:41:21 -0300

    chore: add global middleware to handle the exceptions

--- src/routes/tasks.routes.ts
+++ src/routes/tasks.routes.ts
@@ -10,17 +10,13 @@ const tasksRouter = Router();
 tasksRouter.use(ensureAuthenticated);
 
 tasksRouter.post('/', async (request, response) => {
-	try {
-		const { name, date, user_id } = request.body;
+	const { name, date, user_id } = request.body;
 
-		const createTask = new CreateTaskService();
+	const createTask = new CreateTaskService();
 
-		const task = await createTask.execute({ name, date, user_id });
+	const task = await createTask.execute({ name, date, user_id });
 
-		return response.status(201).json(task);
-	} catch (err) {
-		return response.status(400).json({ error: err.message });
-	}
+	return response.status(201).json(task);
 });
 
 export default tasksRouter;
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -21,19 +21,15 @@ usersRouter.get('/', async (request, response) => {
 });
 
 usersRouter.post('/', async (request, response) => {
-	try {
-		const { email, password } = request.body;
+	const { email, password } = request.body;
 
-		const createUser = new CreateUserService();
+	const createUser = new CreateUserService();
 
-		const user = await createUser.execute({ email, password });
+	const user = await createUser.execute({ email, password });
 
-		delete user.password;
+	delete user.password;
 
-		return response.status(201).json(user);
-	} catch (err) {
-		return response.status(400).json({ error: err.message });
-	}
+	return response.status(201).json(user);
 });
 
 usersRouter.patch(
@@ -41,20 +37,16 @@ usersRouter.patch(
 	ensureAuthenticated,
 	upload.single('avatar'),
 	async (request, response) => {
-		try {
-			const updateUserAvatar = new UpdateUserAvatarService();
+		const updateUserAvatar = new UpdateUserAvatarService();
 
-			const user = await updateUserAvatar.execute({
-				user_id: request.user.id,
-				avatarFilename: request.file.filename,
-			});
+		const user = await updateUserAvatar.execute({
+			user_id: request.user.id,
+			avatarFilename: request.file.filename,
+		});
 
-			delete user.password;
+		delete user.password;
 
-			return response.status(202).json(user);
-		} catch (err) {
-			return response.status(400).json({ error: err.message });
-		}
+		return response.status(202).json(user);
 	},
 );
 
