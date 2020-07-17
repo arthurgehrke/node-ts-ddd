cdcb87027a3e442947ff7ac163e42557cc79729e
arthurgehrke <arthurgehrke07@gmail.com>
2020-07-17 11:51:03 -0300

    feat: add updateUserAvatarService

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -4,7 +4,9 @@ import multer from 'multer';
 import uploadConfig from '../config/upload';
 
 import UsersRepository from '../repositories/UsersRepository';
+
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
 
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 
@@ -38,7 +40,22 @@ usersRouter.patch(
 	'/avatar',
 	ensureAuthenticated,
 	upload.single('avatar'),
-	async (request, response) => {},
+	async (request, response) => {
+		try {
+			const updateUserAvatar = new UpdateUserAvatarService();
+
+			const user = await updateUserAvatar.execute({
+				user_id: request.user.id,
+				avatarFilename: request.file.filename,
+			});
+
+			delete user.password;
+
+			return response.status(202).json(user);
+		} catch (err) {
+			return response.status(400).json({ error: err.message });
+		}
+	},
 );
 
 export default usersRouter;
